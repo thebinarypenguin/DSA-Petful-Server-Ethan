4ff8bcceeccc9c4dc0f281cfc784a5f508d6cfa6
William Y Wong <wwong1987@gmail.com>
2019-04-12 07:49:11 -0700

    added queue and was able to get json data for dog and cat

--- src/server.js
+++ src/server.js
@@ -1,14 +1,58 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 
-const app = express();
-app.use(cors());
+const { CLIENT_ORIGIN } = require('./config');
+const { Queue } = require('./queue');
 
-// Catch-all 404
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const app = express();
+
+const cats = new Queue();
+const dogs = new Queue();
+
+cats.enqueue({
+  imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+
+dogs.enqueue({
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+});
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.get('/api/cat', (req, res, next) => {
+  return res.json(cats.peek());
+});
+
+app.delete('/api/cat', (req, res, next) => {
+  cats.dequeue();
+  return res.sendStatus(204);
+});
+
+app.get('/api/dog', (req, res, next) => {
+  return res.json(dogs.peek());
+});
+
+app.delete('/api/dog', (req, res, next) => {
+  dogs.dequeue();
+  return res.sendStatus(204);
 });
 
 // Catch-all Error handler
