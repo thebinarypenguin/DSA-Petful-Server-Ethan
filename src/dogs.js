324cabb0803620dc8ff08255e7361a8214a9ba36
William Y Wong <wwong1987@gmail.com>
2019-04-12 15:44:41 -0700

    changed descriptions for cats and dogs

--- src/dogs.js
+++ src/dogs.js
@@ -15,41 +15,41 @@ dogs.enqueue({
 dogs.enqueue({
   imageURL: 'https://images.dog.ceo/breeds/beagle/n02088364_13627.jpg',
   imageDescription: 'Dog 2',
-  name: 'Zeus',
+  name: 'Pavlov',
   sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
+  age: 5,
+  breed: 'Mutt',
+  story: 'Owner was a jerk, starved him on purpose for science. Excessive drooler'
 });
 
 dogs.enqueue({
   imageURL: 'https://images.dog.ceo/breeds/shihtzu/n02086240_6463.jpg',
   imageDescription: 'Dog 3',
-  name: 'Zeus',
-  sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
+  name: 'Lassie',
+  sex: 'Female',
+  age: 6,
+  breed: 'Collie',
+  story: 'Very loyal dog. Will notify family if someone falls into a well'
 });
 
 dogs.enqueue({
   imageURL: 'https://images.dog.ceo/breeds/vizsla/n02100583_1318.jpg',
   imageDescription: 'Dog 4',
-  name: 'Zeus',
+  name: 'Rin Tin Tin',
   sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
+  age: 11,
+  breed: 'German Shepherd',
+  story: 'Survived a war and was a former movie star. Will act for treats'
 });
 
 dogs.enqueue({
   imageURL: 'https://images.dog.ceo/breeds/setter-gordon/n02101006_7867.jpg',
   imageDescription: 'Dog 5',
-  name: 'Zeus',
+  name: 'Hachiko',
   sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
+  age: 10,
+  breed: 'Akita',
+  story: 'Waited for previous owner at train station, but owner never came back'
 });
 
 module.exports = dogs;
